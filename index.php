41ffcb1bd5999c9e0e56c1ed768d2b97edf6a606
ViewFromTheBox <jacob.martella@att.net>
2017-07-17 21:24:04 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<div class="row">
+
+    <?php if(have_posts()) : ?>
+        <?php while(have_posts()) : the_post(); ?>
+            <div id="post-<?php the_ID(); ?>" <?php post_class(); ?> style="display: none">
+                <?php the_title('<h2>','</h2>'); ?>
+                <?php the_content(); ?>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
+
+</div>
+
+
+
+
+<?php get_footer(); ?>
